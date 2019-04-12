7e5ba72d4531bc2d120d3524ca66e1be7d22e24c
Antoine Eiche <lewo@abesis.fr>
2019-04-12 17:37:01 +0200

    ci: build PRs

--- ci/jobsets-declarative.nix
+++ ci/jobsets-declarative.nix
@@ -1,7 +1,33 @@
-{ nixpkgs, declInput }:
+{ nixpkgs, declInput, pulls }:
 let
   pkgs = import nixpkgs {};
-  desc = {
+  prs = builtins.fromJSON (builtins.readFile pulls);
+  prJobsets =  pkgs.lib.mapAttrs (num: info:
+    { enabled = 1;
+      hidden = false;
+      description = "PR ${num}: ${info.title}";
+      nixexprinput = "nixexpr";
+      nixexprpath = "ci/jobset.nix";
+      checkinterval = 60;
+      schedulingshares = 20;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 1;
+      inputs = {
+        nixexpr = {
+          type = "git";
+          value = "git://github.com/${info.base.repo.owner.login}/${info.base.repo.name}.git pull/${num}/head";
+          emailresponsible = false;
+        };
+        nixpkgs = {
+          value = "https://github.com/NixOS/nixpkgs f52505fac8c82716872a616c501ad9eff188f97f";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    }
+  ) prs;
+  desc = prJobsets // {
     trunk = {
       description = "Build master of cloudwatt/nix-container-images";
       checkinterval = 60;
